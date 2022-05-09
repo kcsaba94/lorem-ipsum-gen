7a6023142b1ee84ac3668458aac44cc3b88ed735
kcsaba94 <k.csaba994@gmail.com>
2022-05-09 23:49:13 +0200

    Update source code from previous version

--- core.js
+++ core.js
@@ -1,25 +1,31 @@
-let btnGenerate = document.getElementById('loremButton');
-
-btnGenerate.addEventListener('click', function btnIsClicked() {
-    let inputText = document.getElementById('loremInput');
-    let txtResult = document.getElementById('resultTxt');
-
-    let loremObj = 
-        {
-            "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada sed sapien id vulputate. Fusce auctor elit in odio mattis, ut maximus urna elementum. Fusce eget blandit nunc. Mauris nec metus augue. Mauris accumsan hendrerit vulputate. Donec in pellentesque magna. Fusce vel mauris a dolor aliquet congue sit amet ut diam. Praesent rhoncus quam sapien, quis ullamcorper ligula accumsan a. Aliquam tempor tortor quis mi ultrices feugiat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam dictum lacus quis massa consectetur porta."
-        };
-
-    if (isNaN(inputText.value) || inputText.value == '') {
-        txtResult.style.color = "red";
-        txtResult.innerHTML = "A bevitelimező értéke nem megfelelő!";
-        inputText.value = '';
-        inputText.focus();
-    } else {
-        txtResult.style.color = "black"
-        for (let i = 1; i <= inputText.value; i++) {
-            txtResult.innerHTML += loremObj.text + "<br><br>";
+(function () {
+    let btnGenerate = document.querySelector('#loremButton');
+    let btnRemove = document.querySelector('#btnRemove');
+    let inputText = document.querySelector('#loremInput');
+    
+    let createParagraph = () => {
+        if (isNaN(inputText.value) || inputText.value == '') {
+            alert('A megadott érték nem szám, ezért nem felel meg.');
+            inputText.value = '';
+            inputText.focus();
+        } else {
+            for (let i = 1; i <= inputText.value; i++) {
+                const para = document.createElement('p');
+                para.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada sed sapien id vulputate. Fusce auctor elit in odio mattis, ut maximus urna elementum. Fusce eget blandit nunc. Mauris nec metus augue. Mauris accumsan hendrerit vulputate. Donec in pellentesque magna. Fusce vel mauris a dolor aliquet congue sit amet ut diam. Praesent rhoncus quam sapien, quis ullamcorper ligula accumsan a. Aliquam tempor tortor quis mi ultrices feugiat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam dictum lacus quis massa consectetur porta.";
+                document.body.append(para);
+            }
         }
-        inputText.value = '';   
     }
     
-})
+    let removeParagraph = () => {
+        const paras = document.querySelectorAll('p');
+        for (let i = 0; i < paras.length; i++) {
+            paras[i].parentNode.removeChild(paras[i]);
+        }
+        inputText.value = '';
+        inputText.focus();
+    }
+    
+    btnRemove.addEventListener('click', removeParagraph);
+    btnGenerate.addEventListener('click', createParagraph);
+})();
